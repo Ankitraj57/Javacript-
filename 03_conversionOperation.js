208416364c9b70d973466d9fc09c593a5ce53e88
Ankitraj57 <ankitraj1320@gmail.com>
2025-09-15 23:13:12 +0530

    conversion in dataTypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,37 @@
+//  INPUT                      //OUTPUT
+
+// let score = "33"       =      33
+// let score = "33aab"    =      NaN 
+// let score = null       =      0
+// let score = undefined  =      NaN
+// let score = true       =      1
+// let score = false      =      0
+// let score = "Ankit"    =      NaN 
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+/*--------------------*/
+
+// let isLoggedIn = 1 
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "ankit" => true
+
+/*-----------------------------*/
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
