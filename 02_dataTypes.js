a6e4cae81b92e6516749bf6c48b9ba0fd8393761
Ankitraj57 <ankitraj1320@gmail.com>
2025-09-15 22:51:47 +0530

    dataTypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,17 @@
+let name = "Ankit"
+let isLoggedIn = false
+let state;
+
+//------------primitive Data-types-------
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof undefined)
+console.log(typeof null)
