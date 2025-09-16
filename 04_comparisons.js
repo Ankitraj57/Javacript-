1be244e35172be53ad6707659feb8d9fcc4e9ac3
Ankitraj57 <ankitraj1320@gmail.com>
2025-09-16 23:05:07 +0530

    stack and heap

--- 04_comparisons.js
+++ 04_comparisons.js
@@ -21,4 +21,28 @@
 */
 
 // ===
-console.log("2" === 1);
+// console.log("2" === 1);
+
+//---------------------------------------
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubeName = "AnkitKumaryoutube.com"
+let anotherName = myYoutubeName
+
+anotherName = "ankitsinghyoutube.com"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+    email: "userone@gmail.com",
+    upi: "userone@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "ankit@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
