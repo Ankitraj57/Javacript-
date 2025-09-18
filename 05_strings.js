53844ece08c367a03169c643631bc60568eec37d
Ankitraj57 <ankitraj1320@gmail.com>
2025-09-18 11:50:02 +0530

    strings

---
+++ 05_strings.js
@@ -0,0 +1,37 @@
+const name = "Ankit"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("ankit-ak-com")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-7, 4)
+// console.log(anotherString)
+
+// const newStringOne = "  ankit  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://ankit.com/ankit%20kumar"
+
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('ankit'));
+// console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
+
+
+
+
